ae37dfd7704e0bcd7fddab1243e2d34f536b5655
shaayy187 <dawid.puchala2002@gmail.com>
2025-05-19 17:33:35 +0200

    feat(backend): Added consumer to containers. It listens If new person register, If yes, it sends email

--- backend/consumer.py
+++ backend/consumer.py
@@ -1,12 +1,19 @@
 import pika
 import json
+from message import send_welcome_email
 
 def callback(ch, method, properties, body):
     data = json.loads(body)
-    print(f"[x] New user registered: {data['username']} ({data['email']})")
+    username = data['username']
+    email = data['email']
+
+    print(f"[x] New user registered: {username} ({email})")
+
+    send_welcome_email(username, email)
+
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
 channel = connection.channel()
 channel.queue_declare(queue='user_registered_queue', durable=True)
 channel.basic_qos(prefetch_count=1)
