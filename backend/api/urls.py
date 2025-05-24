2be9b82a4563c8017fb7a568448f55927b4df6f6
shaayy187 <dawid.puchala2002@gmail.com>
2025-05-24 15:40:12 +0200

    feat(urls): added new url for updating recipe steps

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,6 +3,7 @@ from .controllers.recipe_controller import RecipeView, RecipeDetailView
 from .controllers.user_controller import Register, UserProfile
 from .controllers.category_controller import CategoryView
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .controllers.step_controller import StepDetailView
 
 urlpatterns = [
     path('recipes/', RecipeView.as_view(), name='recipe-list'),
@@ -12,4 +13,5 @@ urlpatterns = [
     path('category/<int:id>/', CategoryView.as_view(), name='category-detail'),
     path('category/', CategoryView.as_view(), name='category-list'),
     path('user/', UserProfile.as_view(), name='user-profile'),
+    path('steps/<int:id>/', StepDetailView.as_view(), name='step-detail'),
 ]
