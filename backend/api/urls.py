9743f586cacc9a226f40383c832b39b87654ce6b
shaayy187 <dawid.puchala2002@gmail.com>
2025-05-27 19:57:56 +0200

    feat(backend): Added missing refresh token

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 from .controllers.recipe_controller import RecipeView, RecipeDetailView
 from .controllers.user_controller import Register, UserProfile, ChangePasswordView
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .controllers.step_controller import StepDetailView
 from .controllers.category_controller import CategoryListView, CategoryDetailPublicView, CategoryAdminView
-
 
 urlpatterns = [
     path('recipes/', RecipeView.as_view(), name='recipe-list'),
     path('recipes/<int:id>/', RecipeDetailView.as_view(), name='recipe-detail'),
     path('register/', Register.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='login'), 
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('category/', CategoryListView.as_view(), name='category-list'),
     path('category/<int:id>/', CategoryDetailPublicView.as_view(), name='category-detail'),
     path('category/admin/', CategoryAdminView.as_view(), name='category-create'),
