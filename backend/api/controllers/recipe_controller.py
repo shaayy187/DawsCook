18b09f10d49fbbfdb9f46131c508bc0bd3fb0d8c
shaayy187 <dawid.puchala2002@gmail.com>
2025-05-27 21:03:34 +0200

    feat(backend): Added API filtering, sort and pagn

--- backend/api/controllers/recipe_controller.py
+++ backend/api/controllers/recipe_controller.py
@@ -8,34 +8,23 @@ from drf_yasg import openapi
 from rest_framework.exceptions import NotFound
 from ..serializer import RecipeSerializer
 from ..services import recipe_service
+from ..models import Recipe
+from rest_framework.generics import ListAPIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
 
-
-class RecipeView(APIView):
+@swagger_auto_schema(
+    operation_description="Retrieve and create recipes with pagination, filtering and search."
+)
+class RecipeView(ListAPIView):
     permission_classes = [AllowAny]
 
-    @swagger_auto_schema(
-        operation_description="Retrieve all recipes.",
-        responses={
-            200: RecipeSerializer(many=True)
-        }
-    )
-    def get(self, request):
-        recipes = recipe_service.get_recipes()
-        return Response(recipes, status=status.HTTP_200_OK)
-
-    @swagger_auto_schema(
-        operation_description="Create a new recipe.",
-        request_body=RecipeSerializer,
-        responses={
-            201: RecipeSerializer(),
-            400: "Validation error"
-        }
-    )
-    def post(self, request):
-        serializer = RecipeSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filterset_fields = ['difficulty', 'category']
+    ordering_fields = ['rating', 'recipe']
+    search_fields = ['recipe', 'description']
 
 
 class RecipeDetailView(APIView):
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "corsheaders",
     'rest_framework_simplejwt',
     'drf_yasg',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,16 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',  #klasy uprawnień pod view, aby blokować innych
     ],
+
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+    ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+
+    'PAGE_SIZE': 10, 
+
     'EXCEPTION_HANDLER': 'api.exceptions.custom_exception_handler'
 }
 #konfiguracja JWT
