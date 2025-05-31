bb625e7834c47231d4ec7f2eee7ba4113d53f4d0
shaayy187 <dawid.puchala2002@gmail.com>
2025-05-31 15:48:05 +0200

    feat(backend): Created allergy service

---
+++ backend/api/services/allergy_service.py
@@ -0,0 +1,8 @@
+from ..repositories import allergy_repository
+from ..serializer import AllergySerializer
+from rest_framework.exceptions import NotFound
+
+def get_allergies(category_id=None):
+        category = allergy_repository.get_all_allergies()
+        serializer=AllergySerializer(category, many=True)
+        return serializer.data
