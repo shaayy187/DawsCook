56f3d433b338b27451e1cb7076aabc48530adc53
shaayy187 <dawid.puchala2002@gmail.com>
2025-05-31 15:49:28 +0200

    feat(frontend): Implemented React query

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import logo from "./logo.png";
 import SignIn from './SignIn';
 import SignUp from './SignUp';
@@ -13,12 +14,28 @@ import Settings from './Settings';
 import SessionChecker from './Session'
 import CategoryRecipesPage from './CategoryRecipesPage';
 import ChoosenCategoryRecipes from './ChoosenCategoryRecipes'
+import NewRecipe from './NewRecipe'
+
+const queryClient = new QueryClient();
 
 function App() {
   const [recipes, setRecipes] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const token = localStorage.getItem("access") || sessionStorage.getItem("access");
 
   useEffect(() => {
+    fetch("http://localhost:8000/api/user/", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then(res => res.json())
+    .then(data => {
+      setIsAdmin(data.is_superuser);
+    })
+    .catch(() => setIsAdmin(false));
+
     const link = document.createElement('link');
     link.href = 'https://fonts.googleapis.com/css2?family=Alex+Brush&display=swap';
     link.rel = 'stylesheet';
@@ -28,8 +45,6 @@ function App() {
     .then(response => response.json())
     .then(data => setRecipes(data.results))
     .catch(error => console.error("Error:", error));
-
-    const token = localStorage.getItem("token") || sessionStorage.getItem("token");
 
     if (token) {
       setIsLoggedIn(true);
@@ -61,6 +76,7 @@ function App() {
   };
 
   return (
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
     <SessionChecker setIsLoggedIn={setIsLoggedIn} />
       <div className="app">
@@ -71,6 +87,11 @@ function App() {
           </div>
           <div className="navigation-container"> 
             <nav>
+              {isAdmin &&(
+                <>
+                <Link to="/create-recipe">Create recipe</Link>
+                </>
+              )}
               <Link to="/">Home</Link>
               <Link to="/aboutus">About us</Link>
               <Link to="/categories">Recipes</Link>
@@ -88,6 +109,7 @@ function App() {
         <Routes>
           <Route path="/signin" element={<SignIn onLogin={handleLogin} />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/create-recipe" element={<NewRecipe/>}/>
           <Route path="/aboutus" element={<AboutUs />} />
           <Route path="/" element={<Home recipes={recipes} />} />
           <Route path="/recipe/:id" element={<RecipeDetails />} />
@@ -102,6 +124,7 @@ function App() {
         </footer>
       </div>
     </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
