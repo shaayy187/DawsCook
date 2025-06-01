83b029bc4f3ca96e70bbcc6293198e55a301433a
shaayy187 <dawid.puchala2002@gmail.com>
2025-06-01 10:24:49 +0200

    fix(backend): Modified step service to edit steps in front

--- backend/api/services/step_service.py
+++ backend/api/services/step_service.py
@@ -11,3 +11,13 @@ def update_step(step_id, data):
     serializer.is_valid(raise_exception=True)
     updated_step = serializer.save()
     return StepSerializer(updated_step).data
+
+def create_step(data):
+    serializer = StepSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    step = serializer.save()
+    return serializer.data
+
+def delete_step(step_id):
+    step = step_repository.get_step(step_id)
+    step_repository.delete_step(step)
