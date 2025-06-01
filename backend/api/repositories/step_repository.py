020b1cf14be04e8bbf22b7807374ddd9a4e6476a
shaayy187 <dawid.puchala2002@gmail.com>
2025-06-01 10:23:02 +0200

    fix(backend): Modified step repo to edit recipe steps.

--- backend/api/repositories/step_repository.py
+++ backend/api/repositories/step_repository.py
@@ -1,4 +1,5 @@
 from ..models import Step
+from django.shortcuts import get_object_or_404
 
 def get_step_by_id(step_id):
     return Step.objects.filter(id=step_id).first()
@@ -8,3 +9,9 @@ def update_step(step, validated_data):
         setattr(step, attr, value)
     step.save()
     return step
+
+def get_step(step_id):
+    return get_object_or_404(Step, id=step_id)
+
+def delete_step(step):
+    step.delete()
